068b2aaec5cc43dcf04fcd7ccdf6941f477bfbd4
maurelo860@gmail.com <maurelo860@gmail.com>
2025-12-19 10:33:30 -0600

    Se añadio el interfaz para el registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
+import { RegistroPage } from './pages/registro-page/registro-page';
 
 export const routes: Routes = [
     { path: 'login', component: LoginPage },
-    { path: '', redirectTo: 'login', pathMatch: 'full' }, // Por defecto ir al login
-    { path: 'home', component: LoginPage } // Temporal, luego crearemos HomePage
+    { path: 'registro', component: RegistroPage },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'home', component: LoginPage }
 ];
