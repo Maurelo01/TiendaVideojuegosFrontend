7bdc34c3a69382cc62eb33d28e82f889f7166af3
maurelo860@gmail.com <maurelo860@gmail.com>
2025-12-26 21:32:11 -0600

    Se añadieron modificaciones para el manejo de perfiles gamer

--- src/services/usuarioServices/UsuarioService.ts
+++ src/services/usuarioServices/UsuarioService.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UsuarioGamer } from '../../models/usuario-gamer';
 // import { Empresa } from '../../models/empresa'; 
@@ -10,6 +10,10 @@ import { UsuarioGamer } from '../../models/usuario-gamer';
 export class UsuarioService
 {
     private readonly API_URL = 'http://localhost:8080/TiendaVideojuegos/api/usuarios';
+    private httpOptions = 
+    {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
 
     constructor(private http: HttpClient) { }
 
@@ -18,9 +22,9 @@ export class UsuarioService
         return this.http.get<UsuarioGamer>(`${this.API_URL}/gamer/${idUsuario}`);
     }
 
-    actualizarPerfilGamer(idUsuario: number, gamer: UsuarioGamer): Observable<any>
+    actualizarPerfilGamer(idUsuario: number, datos: UsuarioGamer): Observable<any>
     {
-        return this.http.put(`${this.API_URL}/gamer/${idUsuario}`, gamer, { responseType: 'text' });
+        return this.http.put(`${this.API_URL}/gamer/${idUsuario}`, datos, { ...this.httpOptions, responseType: 'text' });
     }
 
     obtenerPerfilEmpresa(idEmpresa: number): Observable<any>
